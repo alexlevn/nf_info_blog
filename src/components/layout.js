d3f31cc6056ad02010cdc4021d951989ac0a5f5e
Alex Lee <dung.le@snapmart.ph>
2022-09-13 15:48:04 +0700

    test comments

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,6 +58,9 @@ const Layout = ({ location, title, children }) => {
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
     >
+      <script defer src="https://cdn.commento.io/js/commento.js"></script>
+      <div id="commento"></div>
+
       <header>{header}</header>
       <main>{children}</main>
       <footer>© {new Date().getFullYear()}</footer>
